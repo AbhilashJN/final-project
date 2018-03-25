b56d6fc00f1c50e80d910989c598b8b405373f96
Abhilash <4bh1.j.10@gmail.com>
2018-03-25 13:13:50 +0000

    Completed eraser script with distance calculation

--- eraser.py
+++ eraser.py
@@ -1,10 +1,25 @@
+import sys
 import csv
+from math import radians, cos, sin, asin, sqrt
+
+def calcDistance(lat1, lon1, lat2, lon2):
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    # 6367 km is the radius of the Earth
+    km = 6367 * c
+    return km
+
 
 f=open('coordsfile.csv','r+')
 reader = csv.reader(f)
 final=[]
 for row in reader:
-	if float(row[0])==14.990593333 and float(row[1])==77.713053333:
+	if calcDistance(float(row[0]),float(row[1]),float(sys.argv[1]),float(sys.argv[2]))>0.2:
 		final.append(row)
 f.seek(0)
 f.truncate()
